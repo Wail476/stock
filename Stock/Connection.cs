78fab64e01f317bebb22e012df4bf52f979c4cec
Wail476 <202392476@student.ttcollege.edu.sa>
2019-12-08 16:06:45 +0300

    Add project files.

---
+++ Stock/Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock
+{
+    public static class Connection
+    {
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\hp\\Documents\\Stockdb.mdf;Integrated Security=True;Connect Timeout=30";
+            return con;
+        }
+    }
+}
